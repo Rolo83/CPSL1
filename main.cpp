f6664275b77a3c5a9bf6a09b0a6131a4919edefc
Rolo83 <michael-a.ryan@analog.com>
2024-04-24 12:22:14 +0000

    fibs series woorking with loops

--- main.cpp
+++ main.cpp
@@ -1,10 +1,34 @@
 #include "mbed.h"
+#include <cstdio>
 
-// a second commit
 
-// main() runs in its own thread in the OS
+int t1 = 0;
+int t2 = 1;
+int next_term = t1 + t2;
+int max_terms = 8;
+
+
+
+
+   //    0, 1, 1, 2, 3, 5, 8, 13, 21... 
+
+
 int main()
 {
+    //this is the main function
+    printf("Fibonacchi sequence: %d, %d, ", t1, t2);
+    for(int i=3; i<max_terms; i++){
+        printf("%d, ", next_term);
+        t1 = t2;
+        t2 = next_term;
+        next_term = t1 + t2;
+
+    }
+
+    printf("\r\n");
+
+
+
     while (true) {
 
     }
